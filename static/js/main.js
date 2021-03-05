7280c56e4a69f4522ae226fd7b0e56c30d2fc489
Abdul Rehman <shahidabdulrehman03@gmail.com>
2021-03-05 19:50:01 +0500

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+var PopOutClass1 = document.getElementById('VideoPop1');
+var PopClass = document.getElementsByClassName('.col-md-3');
+function nameeds() {
+    PopOutClass1.style.width = "100%";
+    PopOutClass1.style.height = "100%";
+}
+function nameedsEX() {
+    PopOutClass1.style.height = "0%";
+ //   PopOutClass.style.transform = "translateX(-100%)";
+}
+
+
+
+
+function NavOpen() {
+    document.getElementById('#links').style.width = "50%";
+}
+function NavClose() {
+    document.getElementById('#links').style.width = "0%";
+}
