ced2e04f577a71ab75763724d94a4ec181164316
Abdul Rehman <shahidabdulrehman03@gmail.com>
2021-03-12 14:15:22 +0500

    Secret Key

--- media/settings.py
+++ media/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=-!y$hdyi!o$nrdp5s!#pe811xus#j=pu4ne^rwxe$d^ecjex^'
+SECRET_KEY = 'Your Secret Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
